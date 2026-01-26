34eda2b82241d250ca5ab597600fcea8689d4eac
UrjitS <urjitsharma12@gmail.com>
2026-01-25 17:09:24 -0800

    Cleaned bluetooth.h

--- bluetooth/bluetooth.h
+++ bluetooth/bluetooth.h
@@ -7,7 +7,6 @@
 #include "device.h"
 #include "agent.h"
 
-
 struct mesh_node;
 
 #define LOCAL_NET_SERVICE_UUID "00001234-0000-1000-8000-00805f9b34fb"
@@ -17,13 +16,12 @@ struct mesh_node;
 #define MAX_DISCOVERED_DEVICES 32
 #define MAX_NAME_SIZE 32
 
-// Callback function types
 typedef void (*ble_discovered_callback)(uint32_t node_id, int16_t rssi);
 typedef void (*ble_connected_callback)(uint32_t node_id);
 typedef void (*ble_disconnected_callback)(uint32_t node_id);
 typedef void (*ble_data_callback)(uint32_t sender_id, const uint8_t *data, size_t len);
 
-/* Tracked device entry */
+// Connected Device Entry (Binc)
 typedef struct {
     uint32_t device_id;
     char mac_address[18];
@@ -35,7 +33,7 @@ typedef struct {
     uint64_t last_seen;
 } tracked_device_t;
 
-/* BLE Node Manager - central structure for managing BLE mesh operations */
+// BLE Node Manager
 typedef struct ble_node_manager {
     Adapter *adapter;
     Agent *agent;
@@ -48,41 +46,33 @@ typedef struct ble_node_manager {
     uint32_t device_id;
     char local_name[MAX_NAME_SIZE];
 
-    /* Tracked devices */
+    // Tracked devices
     tracked_device_t *discovered_devices;
     guint discovered_count;
 
-    /* State */
     gboolean running;
 
-    /* Callbacks */
     ble_data_callback data_callback;
     ble_connected_callback connected_callback;
     ble_disconnected_callback disconnected_callback;
     ble_discovered_callback discovered_callback;
 
-    /* Timer sources */
     guint heartbeat_source;
 } ble_node_manager_t;
 
-/* Initialization and cleanup */
 ble_node_manager_t *ble_init(struct mesh_node *mesh_node, uint32_t device_id, ble_discovered_callback discovered_cb, ble_connected_callback connected_cb, ble_disconnected_callback disconnected_cb, ble_data_callback data_cb);
 gboolean ble_start(ble_node_manager_t *manager);
 void ble_stop(ble_node_manager_t *manager);
 void ble_cleanup(ble_node_manager_t *manager);
 
-/* Main loop */
 void ble_run_loop(ble_node_manager_t *manager);
 void ble_quit_loop(ble_node_manager_t *manager);
 
-/* Data transmission */
 gboolean ble_send_data(ble_node_manager_t *manager, uint32_t dest_id, const uint8_t *data, size_t len);
 gboolean ble_broadcast_data(ble_node_manager_t *manager, const uint8_t *data, size_t len);
 
-/* Connection info */
 guint ble_get_connected_count(ble_node_manager_t *manager);
 void ble_get_connection_table(ble_node_manager_t *manager, uint32_t *devices, guint *count, guint max_count);
 void ble_print_connection_table(ble_node_manager_t *manager);
 
-#endif //LOCALNET_BLUETOOTH_H
-
+#endif // LOCALNET_BLUETOOTH_H
